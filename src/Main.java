410ae9c87788d57a5f9b6cac5b85d69079fcf8fc
DESKTOP-STRK401\hagirainbow <h8a9g11i@gmail.com>
2020-08-10 10:33:03 +0900

    初版作成

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+/**
+ * @author hagirainbow
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("表示できる");
+	}
+
+}
